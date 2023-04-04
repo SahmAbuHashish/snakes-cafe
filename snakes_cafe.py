85ef3cacca11618cd25d963bd91f0471ca9035ab
Sahm Abu Hashish <archi.sahm@gmail.com>
2023-04-04 18:11:23 +0000

    22

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,63 +1,34 @@
-def snakes_cafe():
+def restaurant_menu():
     print("*****************************************")
     print("**     Welcome to the Snakes Cafe! **")
     print("**     Please see our menu below.  **")
     print("**")
     print("** To quit at any time, type " + "quit **")
     print("*******************************************")
-
-    # menu = {
-    #     "Appetizers": ["Wings", "Cookies", "Spring Rolls"],
-    #     "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"],
-    #     "Desserts": ["Ice Cream", "Cake", "Pie"],
-    #     "Drinks": ["Coffee", "Tea", "Unicorn Tears"]
-    # }
-
-    Appetizers= ["Wings", "Cookies", "Spring Rolls"]
-    Entrees= ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"]
-    Desserts= ["Ice Cream", "Cake", "Pie"]
-    Drinks= ["Coffee", "Tea", "Unicorn Tears"]
-
-    print(" ")
-    print("Appetizers")
-    print("----------")
-    for x in Appetizers:
-        print(x)
-
-    print(" ")
-    print("Entrees")
-    print("----------")
-    for x in Entrees:
-        print(x)
-
-    print(" ")
-    print("Desserts")
-    print("----------")
-    for x in Desserts:
-        print(x)
-
-    print(" ")
-    print("Drinks")
-    print("----------")
-    for x in Drinks:
-        print(x)
-
-    All_menu = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado",
-                "A Literal Garden", "Ice Cream", "Cake", "Pie", "Coffee", "Tea", "Unicorn Tears"]
     
-    order = input("What would you like to order?")
-    count = 0
+    menu = {
+        "Appetizers": ["Wings", "Cookies", "Spring Rolls"],
+        "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"],
+        "Desserts": ["Ice Cream", "Cake", "Pie"],
+        "Drinks": ["Coffee", "Tea", "Unicorn Tears"]
+    }
 
-    while order!= "quit":
+
+    for category, items in menu.items():
+        print(f"\n{category.capitalize()}:")
+        for item in items:
+            print(f"- {item}")
     
-      for i in range(len(All_menu)):
-         if order == All_menu[i]:
-            count = count + 1
-            print("${count} order of ${order} has been added to your meal")
-    else:
-        order = input("What would you like to order?")
-
+    print("\nWhat would you like to order?")
+    order = input("> ")
+    while order.lower() != "quit":
+        count = 0
+        if order.lower() in [item.lower() for sublist in menu.values() for item in sublist]:
+            count +=1
+            print(f"{count} order of {order} has been added to your meal")
+        else:
+            print(f"\nWhat would you like to order?")
+        order = input("> ")
     
 
-
-snakes_cafe()
+restaurant_menu()
