0770fad2042c8269137f8c9cecaf0b0bf72cd9c1
Sahm Abu Hashish <archi.sahm@gmail.com>
2023-04-21 05:18:14 +0000

    final2

--- snakes-cafe/snakes_cafe.py
+++ snakes-cafe/snakes_cafe.py
@@ -3,15 +3,16 @@ def restaurant_menu():
     print("**     Welcome to the Snakes Cafe! **")
     print("**     Please see our menu below.  **")
     print("**")
-    print('** To quit at any time, type "quit" **')
+    print("** To quit at any time, type " + "quit **")
     print("*******************************************")
-
+    
     menu = {
         "Appetizers": ["Wings", "Cookies", "Spring Rolls"],
         "Entrees": ["Salmon", "Steak", "Meat Tornado", "A Literal Garden"],
         "Desserts": ["Ice Cream", "Cake", "Pie"],
         "Drinks": ["Coffee", "Tea", "Unicorn Tears"]
     }
+
 
     for category, items in menu.items():
         print(f"\n{category.capitalize()}:")
